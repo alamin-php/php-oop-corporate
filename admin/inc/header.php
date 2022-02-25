b111b7d13c3061dd1825d714d83890d23705403a
Md Al Amin Sarker <53163466+alamin-php@users.noreply.github.com>
2022-02-25 17:37:49 +0600

    added

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -41,11 +41,11 @@
         <div class="grid_12 header-repeat">
             <div id="branding">
                 <div class="floatleft logo">
-                    <img src="img/livelogo.png" alt="Logo" />
+                    
 				</div>
 				<div class="floatleft middle">
-					<h1>Training with live project</h1>
-					<p>www.trainingwithliveproject.com</p>
+					<h1>Corporate</h1>
+					<p>www.corporate.com</p>
 				</div>
                 <div class="floatright">
                     <div class="floatleft">
